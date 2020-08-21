1f7e3df25c44b748194ccd54b30ebfd4527b3595
Simon Tucker <brotherlogic@gmail.com>
2020-08-21 12:19:18 -0700

    Bounce

--- recordsorganiserapi.go
+++ recordsorganiserapi.go
@@ -253,7 +253,7 @@ func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest
 		}
 
 		if len(oldLoc.GetName()) > 0 || len(newLoc.GetName()) > 0 {
-			s.Log(fmt.Sprintf("Updating due to org move: %v", req))
+			s.Log(fmt.Sprintf("Updating due to org move (%v -> %v): %v", oldLoc.GetName(), newLoc.GetName(), req))
 			_, err := s.bridge.updateRecord(ctx, &rcpb.UpdateRecordRequest{Reason: "Org Move Update", Update: &rcpb.Record{Release: &pbgd.Release{InstanceId: req.GetInstanceId()}}})
 			return &rcpb.ClientUpdateResponse{}, err
 		}
