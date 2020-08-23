1ed7b662a50e7791503dc402e95d20eb5769ec4c
Simon Tucker <brotherlogic@gmail.com>
2020-08-23 15:50:29 -0700

    Bounce

--- organisebase.go
+++ organisebase.go
@@ -125,7 +125,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s Server) GetState() []*pbgs.State {
 	return []*pbgs.State{
-		&pbgs.State{Key: "yes", Value: int64(1234)},
+		&pbgs.State{Key: "yes", Value: int64(12345)},
 	}
 }
 
