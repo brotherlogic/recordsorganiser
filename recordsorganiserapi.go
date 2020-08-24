20e2b0734d6b6a60145149cf3917bc52b2229751
Simon Tucker <brotherlogic@gmail.com>
2020-08-23 17:07:47 -0700

    Simplify get location

--- recordsorganiserapi.go
+++ recordsorganiserapi.go
@@ -96,10 +96,7 @@ func (s *Server) GetOrganisation(ctx context.Context, req *pb.GetOrganisationReq
 
 	for _, rloc := range req.GetLocations() {
 		for _, loc := range org.GetLocations() {
-			if loc.GetName() == "Keepers" {
-				s.Log(fmt.Sprintf("KEEP %v", loc))
-			}
-			if utils.FuzzyMatch(rloc, loc) == nil {
+			if rloc.GetName() == loc.GetName() || rloc.GetName() == "" {
 				if req.ForceReorg {
 					n, err := s.organiseLocation(ctx, loc, org)
 					num = n
