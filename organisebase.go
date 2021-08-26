c536f03c9f3ec84da9a9f160aa62533a7e4467b9
simon <brotherlogic@gmail.com>
2021-08-26 11:54:43 -0700

    Adjust val

--- organisebase.go
+++ organisebase.go
@@ -136,7 +136,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s Server) GetState() []*pbgs.State {
 	return []*pbgs.State{
-		&pbgs.State{Key: "yes", Value: int64(12346)},
+		&pbgs.State{Key: "yes", Value: int64(123467)},
 	}
 }
 
