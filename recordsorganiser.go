264a39f6d8b1df787aef6eff70902349b9496152
Simon Tucker <brotherlogic@gmail.com>
2022-01-05 14:23:44 -0800

    Log out the average width

--- recordsorganiser.go
+++ recordsorganiser.go
@@ -138,7 +138,7 @@ func (s *Server) organiseLocation(ctx context.Context, c *pb.Location, org *pb.O
 		overall = append(overall, tfr...)
 	}
 
-	s.Log(fmt.Sprintf("Running split with %v and %v", gaps, len(overall)))
+	s.Log(fmt.Sprintf("Running split with %v and %v -> %v", gaps, len(overall), fwidths))
 	awidth.With(prometheus.Labels{"location": c.GetName()}).Set(float64(fwidths[len(fwidths)/2]))
 	records := s.Split(overall, float32(c.GetSlots()), float32(c.GetQuota().GetTotalWidth()), gaps, c.GetAllowAdjust(), fwidths[len(fwidths)/2])
 	c.ReleasesLocation = []*pb.ReleasePlacement{}
