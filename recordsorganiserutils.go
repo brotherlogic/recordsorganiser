f847b4acd9e6e33e1eba63b15c0ae3fea1183e7d
Simon Tucker <brotherlogic@gmail.com>
2022-02-07 13:00:15 -0800

    Log potential sale

--- recordsorganiserutils.go
+++ recordsorganiserutils.go
@@ -86,6 +86,7 @@ func (s *Server) processAbsoluteWidthQuota(ctx context.Context, c *pb.Location) 
 				break
 			}
 		}
+		s.CtxLog(ctx, fmt.Sprintf("Attempting to sell: %v", r.GetRelease().GetInstanceId()))
 
 		if r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_IN_COLLECTION {
 			up := &pbrc.UpdateRecordRequest{Reason: "org-prepare-to-sell", Update: &pbrc.Record{Release: &pbgd.Release{InstanceId: r.GetRelease().InstanceId}, Metadata: &pbrc.ReleaseMetadata{Category: pbrc.ReleaseMetadata_PREPARE_TO_SELL}}}
